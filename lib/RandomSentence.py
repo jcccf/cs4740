1886c7445eb6c9f5f00adfc911b8cecf3dc8cce0
Marcus <mkl65@cornell.edu>
2012-02-18 16:56:07 -0500

    Finished RandomSentence

--- lib/RandomSentence.py
+++ lib/RandomSentence.py
@@ -1,10 +1,21 @@
+from NGramModel import NGramModel
 
 class RandomSentence():
   def __init__(self, model):
     self.model = model
   
-  def gen_sentence(length):
+  def gen_sentence(self,length):
     # Generates a sentence of length n
     # uses model.get_rand_word
-    pass
+    lst = []
+    for k in xrange(0,length):
+      lst.append(self.model.get_rand_word(lst))
+    return lst
     
+if __name__ == "__main__":
+  mod = NGramModel(2)
+  corpus = [ [ 1, 2, 3, 1, 2, 4, 2, 4 ] ]
+  mod.train(corpus)
+  
+  rs = RandomSentence(mod)
+  print rs.gen_sentence(10)
