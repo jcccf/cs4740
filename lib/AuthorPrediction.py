2c6b03ecffcf1f236e96250a0a3dc742325e001c
Justin Cheng <jccccf@gmail.com>
2012-02-18 17:30:02 -0500

    Updated author prediction

--- lib/AuthorPrediction.py
+++ lib/AuthorPrediction.py
@@ -1,3 +1,5 @@
+from NGramModel import NGramModel
+from WordParser import WordParser
 
 class AuthorPrediction():
   def __init__(self, ngram, smooth_type=None):
@@ -6,13 +8,23 @@ class AuthorPrediction():
     self.ngram = ngram
     self.smooth_type = smooth_type
   
-  def add_author(name):
+  def add_author(self, name, sentences):
     self.author_model[name] = NGramModel(self.ngram, self.smooth_type)
-    raise NotImplementedError("Justin needs to do this")
+    self.author_model[name].train(sentences)
   
-  def predict_author( str ):
+  def predict_author(self, sentences):
     # Given the text, predict which author wrote it
     result = dict()
     for (name,mod) in self.author_model.iteritems():
-      result[ name ] = mod.get_prob(str)
+      result[ name ] = mod.get_prob(sentences[0])
     return max(result, key=result.get)
+
+if __name__ == '__main__':
+  ap = AuthorPrediction(2)
+  co = WordParser('data/EnronDataset/train.txt', 'authors')
+  for author, sentences in co.sentences().iteritems():
+    ap.add_author(author, sentences)
+  co_test = WordParser('data/EnronDataset/test.txt', 'authors')
+  co_test_sentences = co_test.sentences()
+  print co_test_sentences.keys()
+  ap.predict_author(co_test_sentences['beck-s\t'])
