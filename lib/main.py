5b7b135c9578ec1d67c303886ff4de75774c21f4
Marcus <mkl65@cornell.edu>
2012-03-11 12:56:01 -0400

    Added classifiers (Naive Bayes and Maximum Entropy).  NLTK has a hook into WEKA, and I was hoping to use some fancy classifiers in WEKA, but did not get it working yet.

--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,9 @@
 import Parser, Baselinemostfrequentsense, os
+import weka_classifier
+import nltk.classify
 
 print "==CS 4740 Project 2=="
-task = int(input("select method: (baseline most frequent sense=1) "))
+task = int(input("select method: (baseline most frequent sense=1) (naive_bayes=2) (max_ent=3) "))
 
 if task == 1:
     try:
@@ -19,6 +21,26 @@ if task == 1:
                 f.write('%d\n' % element)
         f.close()
     print("done")
-    
+elif task == 2 or task == 3:
+    try:
+        os.makedirs('data/output/')
+    except:
+        pass
+    if task == 2:
+        classifier = weka_classifier.weka_classifier(window_size=10, classifier=nltk.NaiveBayesClassifier)
+    elif task == 3:
+        classifier = weka_classifier.weka_classifier(window_size=10, classifier=nltk.ConditionalExponentialClassifier)
+
+    egs = Parser.load_examples()
+    classifier.train(egs)
+    testdata = Parser.load_data('data/wsd-data/test.data')
+    with open('data/output/mostfreqsense.txt', 'w') as f:
+        for testexample in testdata:
+            prediction = classifier.predict([testexample])
+            for element in prediction:
+                f.write('%d\n' % element)
+        f.close()
+    print("done")
+
         
     
---
+++ lib/weka_classifier.py
@@ -0,0 +1,161 @@
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier, DecisionTreeClassifier
+import Parser,sys,os
+from nltk.classify.weka import ARFF_Formatter
+
+class weka_classifier:
+    def __init__(self,window_size=-1,
+            classifier=nltk.NaiveBayesClassifier,
+            stopwords=nltk.corpus.stopwords.words('english'),
+            split_pre_post=True):
+        self.classifier = classifier
+        self.window_size = window_size
+        self.word_senses = dict()
+        self.egs = dict()
+        self.classifiers = dict()  #trained models
+        self.stopwords = stopwords
+        self.split_pre_post = split_pre_post
+        print self.classifier
+
+    def __tovec(self, d, context, prefix):
+        # Word list to dictoary (sparse vector) converter
+        for w in context:
+            ww = prefix+w
+            if ww in d:
+                d[ww] += 1
+            else:
+                d[ww] = 1
+        return d
+
+    def __stopwords_filter(self, words):
+        # Filters out words in the stopwords list
+        return [ w for w in words if not w in self.stopwords ]
+
+    def __remove_words_in_dict(self, d, words):
+        # Remove <words> from dictionary d
+        for w in words:
+            if w in d:
+                del d[w]
+        return d
+
+    def filter_non_discriminative(self, fvec, limit=0.1):
+        # Filters out words that do not occur in more than <limit> of total examples
+        d = dict()
+        for f, label in fvec:
+#            label = int(label)  # 0 or 1
+            for w in f.iterkeys():
+                if w in d:
+                    d[w] += 1
+                else:
+                    d[w] = 1
+        limit = float(limit) * len(fvec)
+        removed_words = [ w for w,l in d.iteritems() if l < limit ]
+        print len(removed_words),"out of", len(d)
+        new_fvec = [ (self.__remove_words_in_dict(f,removed_words), lab) for (f,lab) in fvec ]
+        return new_fvec
+
+    def __build_eg(self, eg):
+        # Applies features to example.  Currently, only co-occurance and 
+        # target part-of-speech features are included
+        d = dict()  # Sparse feature vector
+        # POS feature
+        d["__POS_"+eg.pos+"__"] = True
+        # co-occurance features:
+        pre_words = eg.context_before.lower().split()
+        post_words = eg.context_after.lower().split()
+        if self.stopwords != None and len(self.stopwords) != 0:
+            pre_words = self.__stopwords_filter(pre_words)
+            post_words = self.__stopwords_filter(post_words)
+        if self.window_size != -1:
+            pre_words = pre_words[-self.window_size:]
+            post_words = post_words[:self.window_size]
+        if self.split_pre_post:
+            self.__tovec(d, pre_words, "-")
+            self.__tovec(d, post_words, "+")
+        else:
+            self.__tovec(d, pre_words, "")
+            self.__tovec(d, post_words, "")
+        return d
+
+    def prepare_examples(self,egs):
+        # Prepares the examples into training data, applying features etc.
+        print "Preparing on %d examples"%len(egs),
+        for eg in egs:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+            eg.word = eg.word.lower()
+            d = self.__build_eg(eg)
+            if not eg.word in self.word_senses:
+                self.word_senses[eg.word] = len(eg.senses)
+            for idx, val in enumerate(eg.senses):
+                if (eg.word,idx) in self.egs:
+                    self.egs[(eg.word, idx)].append( (d, str(val)) )
+                else:
+                    self.egs[(eg.word, idx)] = [ (d,str(val)) ]
+
+    def write_arff(self,save_dir):
+        # Writes the data into (dense) ARFF files for use in WEKA
+        for key,val in self.egs.iteritems():
+            fmt = ARFF_Formatter.from_train(val)
+            word,idx = key
+            fname = save_dir + "%s_%d.arff"%(word,idx)
+            fmt.write(fname, val)
+
+    def train(self,egs):
+        # Trains a classifier for each word sense
+        self.prepare_examples(egs)
+        print "\nTraining on %d wordsenses"%len(self.egs),
+        for key,val in self.egs.iteritems():
+            sys.stdout.write(".")
+            sys.stdout.flush()
+            val = self.filter_non_discriminative(val)
+            if self.classifier == nltk.ConditionalExponentialClassifier:
+                self.classifiers[key] = self.classifier.train(val, sparse=True, trace=0, max_iter=10)
+            else:
+                self.classifiers[key] = self.classifier.train(val)
+
+        print "\nDone"
+        # Clear memory
+        self.egs = None
+
+    def predict(self, egs):
+        # Given a list of examples, predict their word senses
+        res = []
+        for eg in egs:
+            eg.word = eg.word.lower()
+            d = self.__build_eg(eg)
+#            print d
+            n = self.word_senses[eg.word]
+            for idx in range(0,n):
+#                label = self.classifiers[ (eg.word,idx) ].prob_classify(d).prob("1")
+                label = self.classifiers[ (eg.word,idx) ].classify(d)
+                res.append(label)
+        return res
+
+
+if __name__ == '__main__':
+#    classifier = weka_classifier(10,nltk.DecisionTreeClassifier)  # Does not work with sparse features
+#    classifier = weka_classifier(10,nltk.ConditionalExponentialClassifier,split_pre_post=False)
+    classifier = weka_classifier(10,nltk.NaiveBayesClassifier,split_pre_post=False)
+    egs = Parser.load_examples()
+
+    if True:
+        classifier.train(egs)
+#       prediction = classifier.predict( egs[0:3] )
+#       print(prediction)
+        print "True labels vs predicted labels"
+        for eg in egs[0:3]:
+            print eg.word+" ",
+            print eg.senses,
+            print " vs ",
+            print classifier.predict([eg])
+    
+    if False:
+        classifier.prepare_examples(egs)
+        try:
+            os.makedirs('data/weka/')
+        except:
+            pass
+        classifier.write_arff("data/weka/")
+
+    
