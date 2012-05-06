a844ef34efa862db05dc1b55bcb1ce8be004daba
yl478 <yl478@cornell.edu>
2012-05-05 21:09:16 -0400

    ported baseline2

--- lib/Baseline2.py
+++ lib/Baseline2.py
@@ -5,47 +5,60 @@
 
 import nltk, re, cPickle as pickle
 import QuestionParser, DocFilterer, Loader
+from CoreNLPLoader import *
 
 # Helper method that calls the answerer method on all questions
 # answerer should have the signature answerer(question_dict, doclist, doc_posne_list)
-def answer_all(answerer):
+def answer_all(answerer, use_chunk=False):
   with open('data/naive_out.txt', 'w') as fout:
     questions = Loader.questions()
     for qno, question in questions.iteritems():
+      docs = CoreNLPLoader(qno)  
       print qno
-      docs_posne = Loader.docs_posne(qno)
-      docs = Loader.docs(qno)
-      answer = answerer(question, docs, docs_posne)
-      for ans in answer[:5]:
-        fout.write("%d %s\n" % (qno, ans))
+      answer = answerer(question, docs, use_chunk)
+      print answer
+      if answer == None:
+        fout.write("%d top_docs.%d nil\n" % (qno, qno))
+      else:
+        for ans in answer[:5]:
+          fout.write("%d top_docs.%d %s\n" % (qno, qno, ans))
 
 # Search for noun phrases in each sentence in each document, rank documents by # of matched keywords in noun phrases
 # Chunk other noun phrases in each sentence and return as answer
-def naive_answer(question, docs, docs_posne):
+def naive_answer(question, docs, use_chunk=False, doc_limit=50):
   # Get keywords from question
   keywords = QuestionParser.extract_keywords(question['parse_tree'])
-  # print keywords
-
-  # Find sentences that contain keywords, and sort by # of keyword matches
   all_matches = []
-  for i, doc in enumerate(docs_posne):
-    if doc['text'] is not None:
-      matches = DocFilterer.naive_filter_sentences(keywords, doc['text']['sentences'])
-      matches_with_index = [(i, s, c) for s, c in matches]
+  for doc_idx in range(0, min(doc_limit,len(docs.docs)) ):
+    # Loop through each document
+    paragraphs = docs.load_paras(doc_idx)
+    # paragraphs = list of CoreNLPFeatures
+    for paragraph_idx,paragraph in enumerate(paragraphs):
+      tokenized_sentences = paragraph.tokenized()
+      matches = DocFilterer.naive_filter_sentences(keywords, tokenized_sentences)
+      matches_with_index = [(doc_idx, paragraph_idx, s, c) for s, c in matches]
       all_matches += matches_with_index
-  all_matches = sorted(all_matches, key=lambda x: -x[2])
-  
+  all_matches = sorted(all_matches, key=lambda x: -x[3])
   # Extract chunked NPs from sentences
   output_lines = []
-  for i, s, c in all_matches:
-    # print " ".join(docs_posne[i]['text']['sentences'][s]) # Print actual sentence found
-    nps = DocFilterer.naive_extract_nps(keywords, docs_posne[i]['text']['parse_tree'][s])
-    for chunk in DocFilterer.nps_to_chunks(nps):
-      output_lines.append("%s %s" % (docs[i]['docno'], chunk))
-  return output_lines
+  for doc_idx, paragraph_idx, s, c in all_matches:
+    paragraphs = docs.load_paras(doc_idx)
+    paragraph = paragraphs[paragraph_idx]
+    sentence_parse_tree = paragraph.parse_trees(flatten=True)[s]
+    nps = DocFilterer.naive_extract_nps(keywords, sentence_parse_tree)                                      
+    if (use_chunk):
+      return DocFilterer.nps_to_chunks(nps)
+    else:
+      output = []
+      for np in nps:
+          answer = ""
+          for w in np:
+              answer += w[0]+" "
+          output.append(answer.strip())
+      return output
 
 if __name__ == '__main__':
-  answer_all(answerer=naive_answer)
+  answer_all(answerer=naive_answer, use_chunk=False)
   
   # # (Who/what) (is/was) (NE) ? => Find a description of NE
   # if (("Who is" in question['question'] or "Who was" in question['question']) and len(question['pos'])) == 1:
